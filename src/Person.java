64397ceab51477e6eb17dead38b3fd644e013385
ynw99 <yusufedu99@gmail.com>
2021-11-09 11:37:38 +0700

    this keyword

--- src/Person.java
+++ src/Person.java
@@ -3,9 +3,9 @@ class Person {
   String name;
   String address;
 
-  Person(String paramName, String paramAddress) {
-    name = paramName;
-    address = paramAddress;
+  Person(String name, String address) {
+    this.name = name;
+    this.address = address;
   }
 
   Person(String paramName) {
@@ -16,7 +16,7 @@ class Person {
     this(null);
   }
 
-  void sayHello(String paramName) {
-    System.out.println("Hello " + paramName + ". My name is " + name + ".");
+  void sayHello(String name) {
+    System.out.println("Hello " + name + ". My name is " + this.name + ".");
   }
 }
