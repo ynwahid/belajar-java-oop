5a2737eedad85cf0386ab8b8fb1744afd3505cfc
ynw99 <yusufedu99@gmail.com>
2021-11-09 20:14:33 +0700

    getter dan setter method

---
+++ src/programmer/ucup/ganteng/application/CategoryApp.java
@@ -0,0 +1,15 @@
+package programmer.ucup.ganteng.application;
+
+import programmer.ucup.ganteng.data.Category;
+
+public class CategoryApp {
+
+  public static void main(String[] args) {
+
+    var category = new Category();
+    category.setId("ID");
+    category.setId(null);
+
+    System.out.println(category.getId());
+  }
+}
