8fb0c60292c50f932d1a95951117432bdff23ce0
ynw99 <yusufedu99@gmail.com>
2021-11-09 11:09:59 +0700

    method

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,8 @@ class Person {
   final String country = "Indonesia";
   String name;
   String address;
+
+  void sayHello(String paramName) {
+    System.out.println("Hello " + paramName + ". My name is " + name + ".");
+  }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -10,8 +10,13 @@ public class PersonApp {
     System.out.println(person1.address);
     System.out.println(person1.country);
 
+    person1.sayHello("Budi");
+
     Person person2 = new Person();
+
     Person person3;
     person3 = new Person();
+    person3.name = "Eko";
+    person3.sayHello("Budi");
   }
 }
