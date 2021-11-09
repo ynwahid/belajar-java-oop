5ca47686be1fecac47d083ccaadc13f12867a7e8
ynw99 <yusufedu99@gmail.com>
2021-11-09 16:52:38 +0700

    package

---
+++ src/programmer/ucup/ganteng/data/Product.java
@@ -0,0 +1,12 @@
+package programmer.ucup.ganteng.data;
+
+class Product {
+
+  String name;
+  int price;
+
+  Product(String name, int price) {
+    this.name = name;
+    this.price = price;
+  }
+}
