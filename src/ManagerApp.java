aa1fc19253ffefed23a80b9682340d56e787864c
ynw99 <yusufedu99@gmail.com>
2021-11-09 13:41:08 +0700

    object class

--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -8,5 +8,10 @@ public class ManagerApp {
     var vp = new VicePresident("Cakcup");
     vp.name = "Cakcup";
     vp.sayHello("Budi");
+
+    System.out.println(manager);
+    System.out.println(manager.toString());
+    System.out.println(vp);
+    System.out.println(vp.toString());
   }
 }
