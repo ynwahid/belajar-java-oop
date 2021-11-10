508b34b55606acdf5ce0d7257656dc33469b4b13
ynw99 <yusufedu99@gmail.com>
2021-11-10 13:37:30 +0700

    exception

---
+++ src/programmer/ucup/ganteng/application/ValidationApp.java
@@ -0,0 +1,23 @@
+package programmer.ucup.ganteng.application;
+
+import programmer.ucup.ganteng.data.LoginRequest;
+import programmer.ucup.ganteng.error.ValidationException;
+import programmer.ucup.ganteng.util.ValidationUtil;
+
+public class ValidationApp {
+
+  public static void main(String[] args) {
+
+    LoginRequest loginRequest = new LoginRequest("Yusuf", "Rahasia");
+    //    LoginRequest loginRequest = new LoginRequest(null, null);
+
+    try {
+      ValidationUtil.validate(loginRequest);
+      System.out.println("Data valid");
+    } catch (ValidationException | NullPointerException exception) {
+      System.out.println("Data tidak valid: " + exception.getMessage());
+    } finally {
+      System.out.println("Selalu dieksekusi");
+    }
+  }
+}
---
+++ src/programmer/ucup/ganteng/error/ValidationException.java
@@ -0,0 +1,8 @@
+package programmer.ucup.ganteng.error;
+
+public class ValidationException extends Throwable{
+
+  public ValidationException(String message) {
+      super(message);
+  }
+}
